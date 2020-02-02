3719d310120ddfc90dd295ee561ff28f6e28d7e5
Fabrizio Tornatore <torfab94@gmail.com>
2020-02-02 10:59:54 +0100

    beddu cose

---
+++ Assets/Scripts/PuzzleButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleButton : MonoBehaviour
+{
+    public Light l1;
+    public Light l2;
+    public Light l3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
