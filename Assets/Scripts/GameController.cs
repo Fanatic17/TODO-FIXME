8c805794e4d9eded4e8cb65374792df84849ed6b
Giovanni D'Aquino <gio-belle-cars@hotmail.it>
2020-02-02 14:32:27 +0100

    Final per finta

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -62,7 +62,7 @@ public class GameController : MonoBehaviour
 
     public void ChangeScene()
     {
-        SceneManager.LoadScene("Credits");
+        //SceneManager.LoadScene("Credits");
 
     }
 
